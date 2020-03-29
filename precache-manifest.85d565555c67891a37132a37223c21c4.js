ebd52367ef809866d423dc868c43327eab79f99b
epodgorc <enver.podgorcevic@dccs.at>
2020-03-29 19:35:58 +0200

    Updates

---
+++ precache-manifest.85d565555c67891a37132a37223c21c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8eca949372bd7acd62fa3e9fd48fefb",
+    "url": "/disease-spreading-simulation/index.html"
+  },
+  {
+    "revision": "a8c26e5f2d7b5e636276",
+    "url": "/disease-spreading-simulation/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d5d90180fc3d9b0415d",
+    "url": "/disease-spreading-simulation/static/js/2.aad1ee5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/disease-spreading-simulation/static/js/2.aad1ee5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c26e5f2d7b5e636276",
+    "url": "/disease-spreading-simulation/static/js/main.64773e87.chunk.js"
+  },
+  {
+    "revision": "c7a2f45476d82a51d6d6",
+    "url": "/disease-spreading-simulation/static/js/runtime-main.85df1227.js"
+  }
+]);
